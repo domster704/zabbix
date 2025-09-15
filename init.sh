9d19ac59bb15a41fb24c343b3efa4b0229932456
Grigoriy <61056244+domster704@users.noreply.github.com>
2025-09-15 16:03:06 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 openssl rand -hex 32 > ./psk/zabbix-agent.psk
-echo "ZABBIX_AGENT_PSK_ID=$(hostname)-$(hostname -I | awk '{print $1}')" >> .env
+#echo "ZABBIX_AGENT_PSK_ID=$(hostname)-$(hostname -I | awk '{print $1}')" >> .env
+echo "ZABBIX_AGENT_PSK_ID=24f827e4d65ffada7ecee12a0c393dfd8ae8c1235997e06258272f13390fb9f9" >> .env
 
-echo "ZABBIX_AGENT_PSK_ID=$(hostname)-$(hostname -I | awk '{print $1}')"
+#echo "ZABBIX_AGENT_PSK_ID=$(hostname)-$(hostname -I | awk '{print $1}')"
+echo "ZABBIX_AGENT_PSK_ID=24f827e4d65ffada7ecee12a0c393dfd8ae8c1235997e06258272f13390fb9f9"
 echo "PSK=$(< ./psk/zabbix-agent.psk)"
